f29ba42df04dd9d2d9f1e63fc268ad682a0373e9
LilaElefnatTippfehler <andreas.wildmann@gmail.com>
2019-05-06 21:06:20 +0200

    works as a homie sensor now.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,23 @@
-#include <DHTesp.h>
-#include <Arduino.h>
-#include <PubSubClient.h>
-#include <ESP8266WiFi.h>
-#include <ArduinoJson.h>
-#include <Ticker.h>
 #include "config.h"
+#include <DHTesp.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Ticker.h>
+#include "homie.hpp"
 
 #define CLIENTID "ESP8266Client-"
+#define SLEEPSECONDS 60
 
-void callback(char* topic, byte* payload, unsigned int length);
+#define SERIAL false
+
+void callback(char *topic, byte *payload, unsigned int length);
 boolean reconnect();
 void meassurement();
 void sleep();
 
-
 WiFiClient espClient;
-PubSubClient client(MQTT_IP,MQTT_PORT,callback,espClient);
+PubSubClient client(MQTT_IP, MQTT_PORT, callback, espClient);
+Homie homieCTRL = Homie(&client);
 DHTesp dht;
 Ticker dhtMeasure;
 Ticker deepSleep;
@@ -24,32 +26,45 @@ unsigned long lastReconnectAttempt = 0;
 uint8_t flag_measure = 0;
 uint8_t flag_sleep = 0;
 String ClientID;
-const float divider = 0.0092998;    //Divider for Vcc meassurement
+const float divider = 0.0092998; // Divider for Vcc meassurement
 
-void setup(){
+void setup() {
 
         Serial.begin(115200);
         WiFi.mode(WIFI_STA);
-        WiFi.begin(WIFI_SSID,WIFI_PASS);
+        WiFi.begin(WIFI_SSID, WIFI_PASS);
         while (WiFi.status() != WL_CONNECTED) {
                 delay(500);
-                Serial.print(".");
+                if(SERIAL) Serial.print(".");
         }
-        while(!Serial);
-        Serial.println("");
-        Serial.print("Connected, IP address: ");
-        Serial.println(WiFi.localIP());
-        Serial.println();
+        while (!Serial)
+                ;
+        if(SERIAL) Serial.println("");
+        if(SERIAL) Serial.print("Connected, IP address: ");
+        if(SERIAL) Serial.println(WiFi.localIP());
+        if(SERIAL) Serial.println();
 
-        //if((float) analogRead(A0) * divider <= 6.5) ESP.deepSleep(1000000 * 5);
+        HomieDevice homieDevice = HomieDevice(DEVICE_NAME, "Sensorbrick", WiFi.localIP().toString().c_str(),
+                                              WiFi.macAddress().c_str(), FW_NAME, FW_VERSION,
+                                              "esp8266", "");
+        HomieNode dhtNode = HomieNode("air-sensors", "Air Sensors", "DHT22");
+        HomieProperties temperature = HomieProperties("temperature", "Temperature",
+                                                      false, true, "°C",
+                                                      homie::float_t);
+        HomieProperties humidity = HomieProperties("humidity", "Humidity", false,
+                                                   true, "%", homie::float_t);
 
-        Serial.println("Status\tHumidity (%)\tTemperature (C)\tHeatIndex (C)");
-        dht.setup(D1, DHTesp::DHT22); //DHT22 on Pin1
-
+        // if((float) analogRead(A0) * divider <= 6.5) ESP.deepSleep(1000000 * 5);
+        dhtNode.addProp(temperature);
+        dhtNode.addProp(humidity);
+        homieDevice.addNode(dhtNode);
+        homieCTRL.setDevice(homieDevice);
+        if(SERIAL) Serial.println("Status\tHumidity (%)\tTemperature (C)\tHeatIndex (C)");
+        dht.setup(D1, DHTesp::DHT22); // DHT22 on Pin1
 }
 
-void loop(){
-        if (!client.connected()) {
+void loop() {
+        if (!homieCTRL.connected()) {
                 unsigned long now = millis();
                 if (now - lastReconnectAttempt > 5000) {
                         lastReconnectAttempt = now;
@@ -60,81 +75,51 @@ void loop(){
                                 meassurement();
                         }
                 }
-
         }
-        if(flag_measure == 1) {
+        if (flag_measure == 1) {
                 float humidity = dht.getHumidity();
                 float temperature = dht.getTemperature();
 
-                Serial.print(dht.getStatusString());
-                Serial.print("\t");
-                Serial.print(humidity, 1);
-                Serial.print("\t\t");
-                Serial.print(temperature, 1);
-                Serial.print("\t\t");
-                Serial.println(dht.computeHeatIndex(temperature, humidity, false), 1);
+                if(SERIAL) Serial.print(dht.getStatusString());
+                if(SERIAL) Serial.print("\t");
+                if(SERIAL) Serial.print(humidity, 1);
+                if(SERIAL) Serial.print("\t\t");
+                if(SERIAL) Serial.println(temperature, 1);
 
-                const int capacity = JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(4);
-                const int capacityData = JSON_OBJECT_SIZE(3);
-                StaticJsonBuffer<capacity> jb;
-                StaticJsonBuffer<capacityData> jbData;
-                JsonObject& root = jb.createObject();
-                JsonObject& data = jbData.createObject();
-                data["temperature"].set(temperature);
-                data["humidity"].set(humidity);
-                data["heatIndex"].set(dht.computeHeatIndex(temperature, humidity, false));
-
-                root["device"].set(DEVICE_NAME);
-                //root["Vin"].set((float) analogRead(A0) * divider);
-                root["sensor"].set("DHT22");
-                root["data"].set(data);
-
-                String topic = "/" + String(DEVICE_NAME);
-                char* buffer = (char*) malloc(topic.length()+1);
-                topic.toCharArray(buffer, topic.length()+1);
-                String output;
-                root.printTo(output);
-                uint8_t* buffer2 = (uint8_t*) malloc(output.length()+1);
-                output.getBytes(buffer2, output.length()+1);
-                client.publish(buffer, buffer2,output.length()+1,true);
-                free(buffer);
-                free(buffer2);
+                char buffer[6]; //3 char + point + 2 char
+                sprintf(buffer, "%.2f", temperature);
+                string topic = "homie/" + string(DEVICE_NAME) + "/air-sensors/temperature";
+                client.publish(topic.c_str(),buffer,true);
+                sprintf(buffer, "%.2f", humidity);
+                topic = "homie/" + string(DEVICE_NAME) + "/air-sensors/humidity";
+                client.publish(topic.c_str(),buffer,true);
 
                 dhtMeasure.detach();
                 flag_measure = 0;
-                deepSleep.attach(1.0,sleep);
+                deepSleep.attach(1.0, sleep);
         }
-        if(flag_sleep == 1) {
+        if (flag_sleep == 1) {
                 flag_sleep = 0;
+                homieCTRL.sleep();
                 deepSleep.detach();
-                Serial.println("deepSleep");
-                ESP.deepSleep(1000000 * 5);
+                if(SERIAL) Serial.println("deepSleep");
+                ESP.deepSleep(1000000 * SLEEPSECONDS);
         }
-        client.loop();
-
-
+        homieCTRL.loop();
 }
 
-void meassurement(){
+void meassurement() {
         flag_measure = 1;
 }
 
-void sleep(){
+void sleep() {
         flag_sleep = 1;
 }
 
 boolean reconnect() {
         // Loop until we're reconnected
-        if (client.connect(ClientID.c_str(),MQTT_USR,MQTT_PW)) {
-                Serial.println(" MQTT Connected");
-
-        }else{
-                Serial.print("MQTT conncetion failed, rc=");
-                Serial.print(client.state());
-                Serial.println(" try again in 5 seconds");
-        }
-        return client.connected();
+        return homieCTRL.connect(ClientID.c_str(), MQTT_USR, MQTT_PW);
 }
 
-void callback(char* topic, byte* payload, unsigned int length){
+void callback(char *topic, byte *payload, unsigned int length) {
 }
